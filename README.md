83d4efca24849f798e1935d22f4a8e5577a8d0f1
Dylan Vassallo <dylanvassallo@gmail.com>
2013-08-20 15:47:36 -0700

    Rename google-app-engine to gae-sdk

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This repository contains a Homebrew formula for installing the Google App
 Engine SDK with my custom patches. It will be kept up to date with the latest
-SDK version that is known to be acceptable for Khan Academy development.
+SDK version that is known to be acceptable for Khan Academy development. It
+does not conflict with the standard `google-app-engine` formula, although only
+one of these can be `brew link` 'd at a time.
--- gae-sdk.rb
+++ gae-sdk.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class GoogleAppEngine < Formula
+class GaeSdk < Formula
   homepage 'https://developers.google.com/appengine/'
   url 'http://googleappengine.googlecode.com/files/google_appengine_1.8.3.zip'
   sha1 '827468f89c78f292c28ceac50e6109c8d649fa61'
@@ -9,7 +9,7 @@ class GoogleAppEngine < Formula
 
   def install
     cd '..'
-    share.install 'google_appengine' => name
+    share.install 'gae_sdk' => name
     bin.mkpath
     %w[
       _python_runtime.py
